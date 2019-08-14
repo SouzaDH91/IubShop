a9a6b9a02f5c186715655a9a466f21f42c08d01e
Diego Souza <souzadh1991@outlook.com>
2019-08-14 17:44:07 -0300

    Criação de models, controllers e views
    Arquivos de estruturação criados

---
+++ Controllers/ProductController.cs
@@ -0,0 +1,35 @@
+﻿using IubShop.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace IubShop.Controllers
+{
+    public class ProductController : Controller
+    {
+        /*
+         * ActionResult
+         * IActionResult
+         */
+        public ActionResult Show()
+        {
+            Product product = GetProduct();
+
+            return View(product);
+            //return new ContentResult() { Content = "<h3>product -> view</h3>", ContentType = "text/html" };
+        }
+
+        private Product GetProduct()
+        {
+            return new Product()
+            {
+                id = 1,
+                name = "Xbox One X",
+                price = 1565.50,
+                description = "Jogue em 4K com o melhor console da atualidade!"
+            };
+        }
+    }
+}
